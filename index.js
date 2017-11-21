88b7a9cee2c60a44bb31fb7e0362bcdc1ad44bb4
oltion abazi <oltion.abazi@gmail.com>
2017-11-21 16:02:29 +0100

    created profile widget, ignored dist from being versioned, created events template. Still need to fill it tho

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import profile from './data/hub_profile.json';
 import events from './data/hub_events.json';
 import profileTemplate from './templates/profile.handlebars';
+import eventsTemplate from './templates/events.handlebars';
 
 document.addEventListener("DOMContentLoaded",function() {
-    let div = document.createElement('div');
-    div.innerHTML = profileTemplate(profile);
-    document.querySelector('#profile').appendChild(div);
+    let profileContainer = document.createElement('div');
+    profileContainer.innerHTML = profileTemplate(profile);
+    document.querySelector('#profile').appendChild(profileContainer);
+
+    let eventsContainer = document.createElement('div');
+    eventsContainer.innerHTML = eventsTemplate(events);
+    document.querySelector('#events').appendChild(eventsContainer);
 });
